479c9d99801037a62b7de9507d3fd7ae11bfc99b
alexandershubin <shubin8@mail.ru>
2020-02-11 15:32:12 +0300

    add touch for mobile

--- source/js/script.js
+++ source/js/script.js
@@ -5,18 +5,15 @@ const initSlider = () => {
   const overlay = document.querySelector('.overlay');
 
   const onMouseDown = (evt) => {
-    evt.preventDefault();
-
     // координаты курсора мыши на момент начала события
     const startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
+      x: evt.clientX ? evt.clientX : evt.touches[0].clientX ,
+      y: evt.clientY ? evt.clientY : evt.touches[0].clientY
     };
 
     let dragged = false;
 
     const onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
       dragged = true;
       // высота и ширина окна
       const viewportWidth = mainViewport.offsetWidth;
@@ -24,13 +21,13 @@ const initSlider = () => {
 
       // смещение курсора относительно координат начала события
       const shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
+        x: startCoords.x - (moveEvt.clientX ? moveEvt.clientX : moveEvt.touches[0].clientX),
+        y: startCoords.y - (moveEvt.clientY ? moveEvt.clientY : moveEvt.touches[0].clientY)
       };
 
       // координаты передвижения
-      startCoords.x = moveEvt.clientX;
-      startCoords.y = moveEvt.clientY;
+      startCoords.x = moveEvt.clientX ? moveEvt.clientX : moveEvt.touches[0].clientX;
+      startCoords.y = moveEvt.clientY ? moveEvt.clientY : moveEvt.touches[0].clientY;
 
       const topCoordinate = fixOverflow(button.offsetTop - shift.y, button.offsetHeight, viewportHeight);
       const leftCoordinate = fixOverflow(button.offsetLeft - shift.x, button.offsetWidth, viewportWidth);
@@ -40,14 +37,15 @@ const initSlider = () => {
     };
 
     const onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
+
+      document.removeEventListener('touchmove', onMouseMove);
+      document.removeEventListener('touchend', onMouseUp);
 
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
 
       if (dragged) {
         const onClickPreventDefault = function (evt) {
-          evt.preventDefault();
           button.removeEventListener('click', onClickPreventDefault)
         };
         button.addEventListener('click', onClickPreventDefault);
@@ -79,10 +77,15 @@ const initSlider = () => {
       return coordinate;
     };
 
+    document.addEventListener('touchmove', onMouseMove);
+    document.addEventListener('touchend', onMouseUp);
+
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   };
+  button.addEventListener('touchstart', onMouseDown);
   button.addEventListener('mousedown', onMouseDown);
+
 };
 initSlider();
 
